8aa605b51b51fefb408b9c418b7d0a36cf99126c
Rodrigo Lopes <59925590+Kiro-tagama@users.noreply.github.com>
2024-02-06 15:04:25 +0000

    init

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9999
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {res.status(200).json('Hello World!')})
+/* */
+app.get('/clientes/:id/extrato', (req, res) => {
+  var id = req.params.id
+  // verificar se o usuario existe e retornar o extrato
+  res.status(200).json(id)
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/clientes/:id/transacoes', (req, res) => {
+  // verificar se o saldo é menor que o limite
 })
+/* */
+app.listen(port, () => {console.log(`on port ${port}`)})
