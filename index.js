9617f280ffd7be5320b2755fdfb33be96104d861
Rodrigo Lopes <59925590+Kiro-tagama@users.noreply.github.com>
2024-02-08 14:25:35 +0000

    get ok

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express')
+const {Client} = require('pg')
 const app = express()
-const port = 9999
+const port = 3030
 
-app.get('/', (req, res) => {res.status(200).json('Hello World!')})
+const client = new Client({
+  user: 'admin',
+  host: 'localhost',
+  database: 'rinha',
+  password: '123',
+  port: 5432
+})
+client.connect().then(() => console.log("conectado")).catch((err) => console.log(err))
+
+app.use(express.json())
+
 /* */
 app.get('/clientes/:id/extrato', (req, res) => {
   var id = req.params.id
-  // verificar se o usuario existe e retornar o extrato
-  res.status(200).json(id)
+  
+  client.query('SELECT * FROM clientes WHERE id = $1', [id], (err, result) => {
+    if (result.rows.length === 0){res.status(404).json(result.rows[0])}
+
+    client.query('SELECT * FROM transacoes WHERE client_id = $1', [id], (err, trans) => {
+      const extrato = {
+        saldo: {
+          total: result.rows[0].saldo,
+          data_extrato: new Date().toISOString(),
+          limite: result.rows[0].limite
+        },
+        ultimas_transacoes: trans && trans.rows ? trans.rows : []
+      };
+      res.status(200).json(extrato);
+    })
+
+  });
+
 })
 app.post('/clientes/:id/transacoes', (req, res) => {
-  // verificar se o saldo é menor que o limite
+  var id = req.params.id
+  var valor = req.body.valor
+
 })
 /* */
-app.listen(port, () => {console.log(`on port ${port}`)})
+app.listen(port, () => {})
